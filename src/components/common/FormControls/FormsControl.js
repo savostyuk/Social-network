e7e2f96396231fbd1810877c33a69e8891d6ec2b
Natalia Shulzhenko <savostyuk@gmail.com>
2021-01-11 21:33:37 +0300

    form control

--- src/components/common/FormControls/FormsControl.js
+++ src/components/common/FormControls/FormsControl.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import styles from './FormsControl.module.css'
 
-export const Textarea = ({input, meta, ...props}) => {
-
+const FormControl = ({input, meta, ...props}) =>{
     const hasError = meta.touched && meta.error;
-    return <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
+    return (
+        <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
         <div>
-            <textarea {...input} {...props}/>
+            {props.children}
         </div>
         {hasError && <span>{meta.error}</span>}
     </div>
+    )
 }
 
-export const Input = ({input, meta, ...props}) => {
+export const Textarea = (props) => {
+    const {input, meta, child, ...restProps} = props;
+return <FormControl {...props}>
+    <textarea {...input} {...restProps}/>
+</FormControl>
+}
 
-    const hasError = meta.touched && meta.error;
-    return <div className={styles.formControl + " " + (hasError ? styles.error : '')}>
-        <div>
-            <input {...input} {...props}/>
-        </div>
-        {hasError && <span>{meta.error}</span>}
-    </div>
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return <FormControl {...props}>
+    <input {...input} {...restProps}/>
+</FormControl>
 }
