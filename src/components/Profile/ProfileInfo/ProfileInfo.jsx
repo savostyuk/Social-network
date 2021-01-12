72cf366e10d7e97ea86727e695446006f3b93976
Natalia Shulzhenko <savostyuk@gmail.com>
2021-01-12 17:35:41 +0300

    useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) =>{
 
@@ -18,7 +19,7 @@ const ProfileInfo = (props) =>{
                 <img src={props.profile.photos.large}/>
                 <div>{props.profile.aboutMe}</div>
                 <div>{props.profile.fullName}</div>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     )
